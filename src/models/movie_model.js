8e947437330936975b9968ef7dcad295bd6ebb86
Eliana <eliana04112001@gmail.com>
2024-04-08 08:45:07 -0500

    Creación de movie_model.js

--- src/models/movie_model.js
+++ src/models/movie_model.js
@@ -1,1 +1,21 @@
 const mongoose = require("mongoose");
+
+const movieSchema = new mongoose.Schema(
+    {
+        title: { type: String, required: true },
+        description: { type: String, required: true },
+        release_date: { type: String, required: true },
+        genre: { type: Array, required: true },
+        director: { type: Array, required: true },
+        cast: { type: Array, require: true },
+        duration: { type: Number, requiere: true },
+        rating: { type: String, requiere: true },
+        language: { type: String, requiere: true },
+        poster: { type: Array, requiere: true },
+        trailer: { type: String, requiere: true },
+        production_company: { type: String, requiere: true },
+        country: {type: String, requiere: true}
+    }
+)
+
+module.exports = mongoose.model("Movie", movieSchema);
