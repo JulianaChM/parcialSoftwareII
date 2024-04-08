bec033a278e4d888f8507424598d98a8acec4fab
Eliana <eliana04112001@gmail.com>
2024-04-08 08:19:11 -0500

    Conexión base de datos y puerto habilitado

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const mongoose = require("mongoose");
+const cors = require("cors")
+
+
+// poder acceder a leer el archivo .env
+require( 'dotenv' ).config() ;
+const CONNECTION_PORT = process.env.PORT || 3005;
+app.listen(CONNECTION_PORT,()=>{
+    console.log(`Server running on port ${CONNECTION_PORT}`);
+[ ] });
+
+const mongo_connect = ()=>{
+    try{
+        mongoose
+        .connect(process.env.DATABASE_CONNECTION_STRING)
+        .then(() =>{
+            console.log("Connected to the database")
+        })
+        .catch((error)=>{
+            console.log("Error connecting to the database: ", error);
+        })
+    }catch(err){
+        console.log("Connection Error!",err);
+
+
+    }
+};
+
+
+mongo_connect();
